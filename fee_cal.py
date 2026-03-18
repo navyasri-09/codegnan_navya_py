306be5444a8fd7c8c3d3086de9e02b8d1d15d3c7
navyasri-09 <medisettinavyasri@gmail.com>
2026-03-18 21:39:50 +0530

    Fee calculator

--- fee_cal.py
+++ fee_cal.py
@@ -1,9 +1,12 @@
 name=input("Enter student name:")
+print("---------------------->")
 admission_fee=int(input("Enter Admisission fee:"))
 tuition_fee=int(input("Enter Tuition fee:"))
 hostel_fee=int(input("Enter Hostel fee:"))
 total_fee=admission_fee+tuition_fee+hostel_fee
+print("---------------------->")
 print("Student Name:",name)
 print("Admission fee:",admission_fee)
-print("Tuition Fee:",hostel_fee)
+print("Tuition Fee:",tuition_fee)
+print("Hostel Fee:",hostel_fee)
 print("Total fee:",total_fee)
