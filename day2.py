288fff1864e7d0d1f3e7560ebda8d00540e68ca7
navyasri-09 <medisettinavyasri@gmail.com>
2026-03-18 13:09:26 +0530

    datatypes and fee calculator along with CLI

---
+++ day2.py
@@ -0,0 +1,72 @@
+'''
+variables are name storage loacation used to store the data
+rule:
+A-Z,a-z,0-9
+can start with uppercase lowercase underscore
+can't start with spcl char(@,$,*) and numbers
+'''
+a=5
+b=1
+a=25
+name="Codegnan"
+location="Visakhapatnam"
+age=7
+emailid="com@codegnan.com"
+email_id="com@codegnan.com"
+print(name,location,age,email_id)
+print(name,location,age,emailid)
+
+kavya,jai,ram=21,23,24
+print(kavya)
+print(jai)
+print(ram)
+
+#
+#if=23 invalid syntax
+#
+#lambda="navya" invalid syntax
+#
+#False=0 invalid syntax
+false=0
+'''built in datatypes -- numeric,boolean,collections
+ numeric datatypes --- int,float,complex
+'''
+count=40
+print(count)
+print(type(count))
+
+price=45.65
+print(price)
+print(type(price))
+
+j3=25
+value=2+j3
+print(value)
+print(type(value))
+
+value=2+3j
+print(value)
+print(type(value))
+
+b=float(age)
+print(b)
+print(type(b))
+
+a=True
+print(a)
+print(type(a))
+
+b=int(a)
+print(b)
+
+c=float(a)
+print(c)
+d=complex(float(int(True))
+print(d)
+print(type(d))
+
+a=float(input("Enter a num:"))
+print(a)
+print(type(a))
+
+
---
+++ fee_cal.py
@@ -0,0 +1,9 @@
+name=input("Enter student name:")
+admission_fee=int(input("Enter Admisission fee:"))
+tuition_fee=int(input("Enter Tuition fee:"))
+hostel_fee=int(input("Enter Hostel fee:"))
+total_fee=admission_fee+tuition_fee+hostel_fee
+print("Student Name:",name)
+print("Admission fee:",admission_fee)
+print("Tuition Fee:",hostel_fee)
+print("Total fee:",total_fee)
