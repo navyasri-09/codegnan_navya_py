6a5dcd9e3a51decb3c373331f0b063a987df660b
navyasri-09 <medisettinavyasri@gmail.com>
2026-03-24 13:10:41 +0530

    String data type an methods()

---
+++ day5.py
@@ -0,0 +1,71 @@
+'''
+string--- it is a collection of characters which is reprented by " " or ' '
+anything in b/w quotes comes under string
+
+str="Python Programming"
+print(str)
+
+#we can also use index to find thespecific element in sequence
+index palcement[]
+str="Python Programming"
+print(str[7])
+
+str="Python Programming"
+print(str[7:13])
+
+str="Python Programming"
+print(str[:13])
+
+#immutable,we can't modify  on pqrticular var
+str="Python Programming"
+mod=str.replace("Python","Java")
+print(str)
+print(mod)
+
+#negative indexing access from back starts with index [1]
+str="Python Programming"
+print(str[-6])
+
+a_day="I'm Navya Sri from Visakhapatnam completed my Btech in Raghu Engineering College in the year 2025, currently a Data analytics trainee"
+print(f"My name is {a_day[4:13]}")
+print(f"My course is {a_day[-22:-1]}")
+print(a_day[: : -2])
+
+len()---to get the length of the string
+
+str='123'
+num=int(str)
+print(type(num))
+
+str="Python Programming"
+print(len(str))  #length of a string
+
+#slpit()---remove space, and it is converted into list[] it will give the seperated thing in each index
+str="Python Programming   is a programming language"
+print(str.split(" "))
+print(str.split("   "))
+print(str.split("Progamming"))
+
+#lower()---covert string to lowercase char
+str="pyTHon pRograMminG"
+print(str.lower())
+
+#upper()---covert string to uppercase char
+str="pyTHon pRograMminG"
+print(str.upper())
+
+#replace()--used to replace old string with new string
+str="Python Programming"
+print(str.replace("Python","Java"))
+'''
+
+str="python is a programming language"
+if "python" in str:
+    print("Yes")
+else:
+    print("NO")
+
+#find out user enterd letter is vowel or not
+    
+
+
